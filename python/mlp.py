mlp: Add a generalized backpropagation alg + bug fixes
 * Fixed annoying bug with Numpy array broadcasting/summing
 * Cleaned up notes into `notes.md`

python/mlp.py
@@ -7,6 +7,8 @@ from matplotlib.cm import get_cmap
 from matplotlib.colors import Normalize
 from matplotlib.gridspec import GridSpec
 
+DBG = 0
+
 
 def activation(arr):
     return 1 / (1 + np.exp(-1 * arr))
@@ -20,7 +22,7 @@ def activation_prime(arr):
 
 
 class MLP:
-    def __init__(self, n_inputs=2, hl=[4, 4], n_outputs=1):
+    def __init__(self, n_inputs=2, hl=[4, 4], n_outputs=1, init_lr=0.1):
         self.n_inputs = n_inputs
         self.n_outputs = n_outputs
         self.n_hl = len(hl)
@@ -31,35 +33,64 @@ class MLP:
         for i in range(1, 2 + self.n_hl):
             self.W.append(np.random.random((self.layers[i], self.layers[i - 1])))
             self.B.append(np.zeros(shape=(self.layers[i], 1)))
-        #
-        # example for 2 hidden layers
-        # W0 = np.random.random((hl[0], n_inputs))
-        # W1 = np.random.random((hl[1], hl[0]))
-        # W2 = np.random.random((n_outputs, hl[1]))
-        # b0 = np.zeros(shape=(hl[0], 1))
-        # b1 = np.zeros(shape=(hl[1], 1))
-        # b2 = np.zeros(shape=(n_outputs, 1))
-        self.lr = 0.1
+
+        self.lr = init_lr
 
     def forward(self, X):
         Z_compute_graph = []
 
-        activations = [X]  # X is the input layer, shape=(n_inputs, 1)
+        activations = [X]  # A[0]=X is the input layer, shape=(n_inputs, 1)
         for i in range(self.n_hl + 1):
-            Z_compute_graph.append(self.W[i] @ activations[-1] + self.B[i])
+            Z = self.W[i] @ activations[-1]  # Stupid numpy reshaping
+            # without a reshape into (-1, 1) this would broadcast into a
+            # Z.size, Z.size array when you try to add it to the bias vector
+            # (Z.size,) and (Z.size, 1) are different!!!
+            Z_compute_graph.append(Z.reshape(-1, 1) + self.B[i])
             activations.append(activation(Z_compute_graph[-1]))
 
-            # # ===***=== Forward pass ===***===
-            # # example for 2 hidden layers
-            # Z1 = model.W[0] @ X + model.B[0]  # (n_hl_1, n_inputs) x (n_inputs, 1) + (n_hl_1, 1)
-            # A1 = activation(Z1)  # (n_hl_1, 1)
-            # Z2 = model.W[1] @ A1 + model.B[1]  # (n_hl_2, n_hl_1) x (n_hl_1, 1) + (n_hl_2, 1)
-            # A2 = activation(Z2)  # (n_hl_2, 1)
-            # Z3 = model.W[2] @ A2 + model.B[2]  # (n_outputs, n_hl_2) x (n_hl_2, 1) + (n_outputs, 1)
-            # A3 = activation(Z3)  # (n_outputs, 1)
-            # #            ===***===
-
         return Z_compute_graph, activations
+
+    def backward(self, m, Y, Z, A):
+        """
+        m - number of training examples
+        X - inputs
+        Y - Outputs
+        Z - WX + b
+        A - Activations(Z)
+        """
+        delta = []
+        # TODO: test with n_outputs > 1 in case we have to average here or
+        # something
+        dC_dA = [A[-1] - Y]  # Error of last layer
+        dC_dW = []
+        dC_db = []
+
+        for i in range(self.n_hl, -1, -1):
+            d = dC_dA[-1] * activation_prime(Z[i])
+            w = (1 / m) * (d @ A[i].T)
+            b = (1 / m) * np.sum(d, axis=1, keepdims=True)
+            a = self.W[i].T @ d
+            delta.append(d)
+            dC_dW.append(w)
+            dC_dA.append(a)
+            dC_db.append(b)
+
+            if DBG:
+                print(
+                    f"[DBG] layer #{i}\n"
+                    f"\n{delta[-1].shape=}"
+                    f"\n{dC_dW[-1].shape=}"
+                    f"\n{dC_dA[-1].shape=}"
+                    f"\n{dC_db[-1].shape=}"
+                )
+
+        # Now we do the model update step
+        dC_dW.reverse()
+        dC_db.reverse()
+
+        for i in range(self.n_hl + 1):
+            self.W[i] -= self.lr * dC_dW[i]
+            self.B[i] -= self.lr * dC_db[i]
 
     def dump_matrices(self, X, Y, figname, show=False):
         """For each layer, output a picture of the weights and biases."""
@@ -297,5 +328,3 @@ class MLP:
             plt.close(fig)
 
         return fig, ax
-
-    # -- ---

python/xor.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from mlp import *
+
+DBG = 0
 
 plt.style.use("./computermodern.mplstyle")
 matplotlib.rcParams.update(
@@ -49,10 +52,12 @@ model = MLP()
 
 m = n_examples
 for t in range(n_examples):
-    print(
-        f"==== Training Step {t}\n{model.W[0]=},\n{model.W[1]=},\n{model.W[2]=},\n{model.B[0]=},\n{model.B[1]=},\n{model.B[2]=},\n{model.lr=}\n"
-    )
-    print("=== *** ===")
+    if DBG:
+        print(
+            f"==== Training Step {t}\n{model.W[0]=},\n{model.W[1]=},\n{model.W[2]=},\n{model.B[0]=},\n{model.B[1]=},\n{model.B[2]=},\n{model.lr=}\n"
+        )
+        print("=== *** ===")
+
     X = X_train[t].reshape(model.n_inputs, 1)
     Y = Y_train[t].reshape(model.n_outputs, 1)
 
@@ -67,38 +72,8 @@ for t in range(n_examples):
     print(f"Training step #{t}\nAvg MSE loss={C}\n")
     #        ===***===
 
-    # ===***=== Backprop ===***===
-    """
-    delta_3 = d(C)/d(Z3) = [d(C)/d(A3)] . sigmoid'(Z3)  # special for output layer
-    """
-
-    delta_3 = (A3 - Y) * activation_prime(Z3)
-    dC_dW3 = (1 / m) * (delta_3 @ A2.T)
-    dC_db3 = (1 / m) * np.sum(delta_3, axis=1, keepdims=True)
-
-    dC_dA2 = model.W[2].T @ delta_3
-    delta_2 = dC_dA2 * activation_prime(Z2)
-    dC_dW2 = (1 / m) * (delta_2 @ A1.T)
-    dC_db2 = (1 / m) * np.sum(delta_2, axis=1, keepdims=True)
-
-    dC_dA1 = model.W[1].T @ delta_2
-    delta_1 = dC_dA1 * activation_prime(Z1)
-    print(f"xxxx {delta_1.shape=}{X.shape=}")
-    dC_dW1 = (1 / m) * (delta_1 @ X.T)
-    dC_db1 = (1 / m) * np.sum(delta_1, axis=1, keepdims=True)
-    #         ===***===
-
-    # ===***=== Gradient descent update ===***===
-    model.W[2] -= model.lr * dC_dW3
-    model.W[1] -= model.lr * dC_dW2
-    print(f"xxxx {(model.lr * dC_dW2).shape=}")
-    print(f"xxxx {(model.lr * dC_dW1).shape=}")
-    model.W[0] -= model.lr * dC_dW1
-
-    model.B[2] -= model.lr * dC_db3
-    model.B[1] -= model.lr * dC_db2
-    model.B[0] -= model.lr * dC_db1
-    #                   ===***===
+    # ===***=== Backprop & Step ===***===
+    model.backward(m, Y, Z=Z_compute_graph, A=activations)
 
     # Make an image of the current model state
     model.graph_out(X, filename=f"step_{t}", show=False)
@@ -106,5 +81,6 @@ for t in range(n_examples):
 
 test = np.array([0, 0])
 _, a = model.forward(test)
-print("\n\n\n")
+print(f"{a=}\n\n\n")
+
 print(f"Model prediction for {test=}: {a[-1][-1]=}")